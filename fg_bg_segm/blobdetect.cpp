1072c0d9553dbcc887e7c439576899172b89f333
Beagle <ubuntu@omap.(none)>
2012-04-07 16:15:09 -0500

    Added more command line options to fg_bg_segm.

--- fg_bg_segm/blobdetect.cpp
+++ fg_bg_segm/blobdetect.cpp
@@ -28,21 +28,28 @@ int main(int argc, char * argv[])
     unsigned int frame_width = 320;
     unsigned int frame_height = 240;
     unsigned int percentage = 5;
-    unsigned int min_dist_sqrd_change = 3000;
+    unsigned int min_dist_sqrd_change = 5;
+    unsigned int bg_subtractor_history = 50;
+    unsigned int bg_subtractor_threshold = 16;
+    
+    bool bg_subtractor_shadow_detect = false;
 
     if (argc == 1) {
         cout << "DEFAULT ";
     }
     // No error checking here, probably should...
-    else if (argc == 5) {
+    else if (argc == 7) {
         frame_width = atoi(argv[1]);
         frame_height = atoi(argv[2]); 
         percentage = atoi(argv[3]);
         min_dist_sqrd_change = atoi(argv[4]);
+        bg_subtractor_history = atoi(argv[5]);
+        bg_subtractor_threshold = atoi(argv[6]);
     }
     else {
         cout << "Usage: ./blobdetect <width> <height> <percentage> "
-            << "<min dist sqrd change>" << endl;
+            << "<min dist sqrd change> <bg subtractor history> "
+            << "<bg subtractor threshold>" << endl;
         cout << "\tIf no parameters are specified, " <<
             "will use defaults." << endl;
         cout << "\twidth: resolution width" << endl;
@@ -51,6 +58,10 @@ int main(int argc, char * argv[])
         cout << "\tmin dist sqrd change: minimum change in distance (squared) "
             << "between new point and old point to constitute a change "
             << "in position" << endl;
+        cout << "\tbg subtractor history: length of the history in the "
+            << "background subtractor" << endl;
+        cout << "\tbg subtractor threshold: threshold of the bg/fg "
+            << "segmentation" << endl;
         return 0;
     }
     
@@ -60,6 +71,8 @@ int main(int argc, char * argv[])
     cout << "\tPercentage of blobs used = " << percentage << endl;
     cout << "\tMin Dist (squared) for change = " 
         << min_dist_sqrd_change << endl;
+    cout << "\tBG Subtractor History = " << bg_subtractor_history << endl;
+    cout << "\tBG Subtractor Threshold = " << bg_subtractor_threshold << endl;
     
     // Set up serial port
     serial_setup();
@@ -87,8 +100,8 @@ int main(int argc, char * argv[])
     cvNamedWindow("fgmask", CV_WINDOW_AUTOSIZE);
     cvMoveWindow("fgmask", frame_width, 0);
     
-    cvNamedWindow("bgimg", CV_WINDOW_AUTOSIZE);
-    cvMoveWindow("bgimg", frame_width<<1, 0);
+    //cvNamedWindow("bgimg", CV_WINDOW_AUTOSIZE);
+    //cvMoveWindow("bgimg", frame_width<<1, 0);
     
     cvNamedWindow("facedetect", CV_WINDOW_AUTOSIZE);
     cvMoveWindow("facedetect", frame_width<<2, 0);
@@ -102,7 +115,8 @@ int main(int argc, char * argv[])
 
     // (int history, float varThreshold, bool bShadowDetection=1)
     // length of the history, varThreshold (typical val is 4 sigma, 4*4=16), ...
-    BackgroundSubtractorMOG2 bg_model(50, 5, 0);
+    BackgroundSubtractorMOG2 bg_model(bg_subtractor_history, 
+        bg_subtractor_threshold, bg_subtractor_shadow_detect);
 
     bool quit = false;
     
@@ -111,29 +125,31 @@ int main(int argc, char * argv[])
     
     // Main loop
     while (!quit&&cvGrabFrame(capture)) {
+        double t = (double) cvGetTickCount();
+
         img = cvRetrieveFrame(capture);
 
         cvConvertScale(img, frame, 1, 0);
-
-        double t = (double) cvGetTickCount();
 
         Mat fgmask, bgimg;
         Mat img_mat = Mat(img, true);
         // Learn current fg/bg: learning is always on (-1)
         bg_model(img_mat, fgmask, -1);
         // Add to average background image
-        bg_model.getBackgroundImage(bgimg);
+        //bg_model.getBackgroundImage(bgimg);
 
         // Show foreground mask
         imshow("fgmask", fgmask);
         // Show the averaged background image
-        imshow("bgimg", bgimg);
+        //imshow("bgimg", bgimg);
 
+        /*
         t = (double) cvGetTickCount () - t;
         printf( "bg/fg segmentation time = %g ms \n",
             t/((double)cvGetTickFrequency() * 1000.) );
         
         t = (double) cvGetTickCount();
+        */
 
         // Convert frame to have 8-bit depth, 1 channel (grayscale)
         IplImage fgmask_ipl = fgmask;
@@ -149,6 +165,8 @@ int main(int argc, char * argv[])
         CvBlobs blobs;
         unsigned int result = cvLabel(segmentated, labelImg, blobs);
 
+        cout << "blobs found: " << blobs.size() << endl;
+
         // Sort by largest area
         vector< pair<CvLabel, CvBlob*> > blobList;
         copy(blobs.begin(), blobs.end(), back_inserter(blobList));
@@ -157,9 +175,9 @@ int main(int argc, char * argv[])
         // Consider at max only the 1/FRAC_BLOBS_TO_RENDER largest blobs
         // TODO what about the case of multiple people? 
         CvBlobs blobs2;
-        unsigned int cur_avg_x = 0;
-        unsigned int cur_avg_y = 0;
-        // FIXME careful with the division/mult here...
+        double cur_avg_x = 0;
+        double cur_avg_y = 0;
+        
         for (int i = 0; i < (blobList.size() * percentage)/100; i++) {
             blobs2.insert(blobList[i]);
             cur_avg_x += /*(*blobList[i].second).area * */(*blobList[i].second).centroid.x;
@@ -178,18 +196,22 @@ int main(int argc, char * argv[])
             cur_avg_y /= blobs2.size();
         }
 
+        cout << "blobs used: " << (blobs2.size() * percentage)/100 << endl;
+        
+        /*
         t = (double) cvGetTickCount() - t;
         printf( "blob detection/selection time = %g ms\n",
             t/((double)cvGetTickFrequency()*1000.));
+        */
 
         // Render blobs 
         cvRenderBlobs(labelImg, blobs2, frame, frame, CV_BLOB_RENDER_BOUNDING_BOX);
 
         // See if the average point has moved significantly enough to warrant a change.
-        unsigned int x_diff = cur_avg_x - avg_x;
-        unsigned int y_diff = cur_avg_y - avg_y;
-        unsigned int dist_sqrd = x_diff * x_diff + y_diff * y_diff;
-        
+        double x_diff = cur_avg_x - avg_x;
+        double y_diff = cur_avg_y - avg_y;
+        double dist_sqrd = x_diff * x_diff + y_diff * y_diff;
+
         if (dist_sqrd > min_dist_sqrd_change) {
             avg_x = cur_avg_x;
             avg_y = cur_avg_y;
@@ -199,7 +221,7 @@ int main(int argc, char * argv[])
         cvCircle(frame, cvPoint(avg_x, avg_y), 5, CV_RGB(0, 0, 255), 3, 8, 0);
 
         // face detection
-        detectAndDraw( img_mat );
+        //detectAndDraw( img_mat );
 
         // Send avg coordinates to arduino
         uint16_t x_coord, y_coord;
@@ -229,6 +251,10 @@ int main(int argc, char * argv[])
         }
         */
 
+        t = (double) cvGetTickCount() - t;
+        printf( "total frame processing time = %g ms\n",
+            t/((double)cvGetTickFrequency()*1000.));
+        
         cvShowImage("motion_tracking", frame);
 
         // Release objects
